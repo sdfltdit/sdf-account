2328ce448433f76b1cf00b9062e17deb16fdc0ed
sdfltdit <126612571+sdfltdit@users.noreply.github.com>
2026-06-11 16:09:13 +0600

    Add experimental config for missingSuspenseWithCSRBailout

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   output: 'export',
   trailingSlash: true,
   images: { unoptimized: true },
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
 }
 
 module.exports = nextConfig
