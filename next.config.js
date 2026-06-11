e29c63d643f210b2a454ce24d136b2a0d5e21aea
sdfltdit <126612571+sdfltdit@users.noreply.github.com>
2026-06-11 18:54:26 +0600

    Simplify next.config.js by removing unused options
    Removed output and experimental settings from configuration.

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  trailingSlash: true,
   images: { unoptimized: true },
-  experimental: {
-    missingSuspenseWithCSRBailout: false,
-  },
 }
 
 module.exports = nextConfig
